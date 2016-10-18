a385112c0cce3faabdfa295ac73cf00d82634f47
Dalgona <dalgona@hontou.moe>
2016-10-18 13:24:11 +0900

    [WIP] refactor
    Serum.PageBuilder

--- lib/serum/build/page_builder.ex
+++ lib/serum/build/page_builder.ex
@@ -2,52 +2,53 @@ defmodule Serum.Build.PageBuilder do
   alias Serum.Build.Renderer
 
   def run(src, dest, mode) do
-    template = Serum.get_data "template_page"
-    info = Serum.get_data :pageinfo
-
-    case mode do
-      :parallel ->
-        info
-        |> Enum.map(&(Task.async __MODULE__, :page_task, [src, dest, &1, template]))
-        |> Enum.each(&(Task.await &1))
-      _ ->
-        info
-        |> Enum.each(&(page_task src, dest, &1, template))
-    end
+    template = Serum.get_data("template_page")
+    info = Serum.get_data(:pageinfo)
+    launch(src, dest, info, template, mode)
   end
+
+  defp launch(src, dest, info, template, :parallel), do:
+    info
+    |> Enum.map(&(Task.async __MODULE__, :page_task, [src, dest, &1, template]))
+    |> Enum.each(&(Task.await &1))
+
+  defp launch(src, dest, info, template, :sequential), do:
+    info
+    |> Enum.each(&(page_task src, dest, &1, template))
 
   def page_task(src, dest, info, template) do
     srcname = "#{src}pages/#{info.name}.#{info.type}"
     dstname = "#{dest}#{info.name}.html"
 
-    txt = File.read! srcname
-    html = render(txt, info, template)
+    html = srcname
+           |> File.read!
+           |> render(info.type, info.title, template)
 
-    subdir = get_subdir info.name
-    if get_subdir(info.name) != "", do: File.mkdir_p! "#{dest}#{subdir}"
-    File.open! dstname, [:write, :utf8], fn device ->
-      IO.write device, html
-    end
+    subdir = get_subdir(info.name)
+    if subdir != "", do: File.mkdir_p!("#{dest}#{subdir}")
+    File.open!(dstname, [:write, :utf8], fn device ->
+      IO.write(device, html)
+    end)
 
     IO.puts "  GEN  #{srcname} -> #{dstname}"
   end
 
-  defp render(txt, info, template) do
-    html = case info.type do
-      "md" -> Earmark.to_html txt
-      "html" -> txt
-    end
+  defp render(md, "md", title, template), do:
+    template
+    |> Renderer.render([contents: Earmark.to_html(md)])
+    |> Renderer.genpage([page_title: title])
+
+  defp render(html, "html", title, template), do:
     template
     |> Renderer.render([contents: html])
-    |> Renderer.genpage([page_title: info.title])
-  end
+    |> Renderer.genpage([page_title: title])
 
   defp get_subdir(path) do
     [_|subdir] = path |> String.split("/") |> Enum.reverse
     case subdir do
-      [] -> ""
+      []  -> ""
       [x] -> x <> "/"
-      x when is_list x -> (x |> Enum.reverse |> Enum.join("/")) <> "/"
+      l   -> (l |> Enum.reverse |> Enum.join("/")) <> "/"
     end
   end
 end
