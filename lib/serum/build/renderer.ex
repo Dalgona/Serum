60faf00c32885162f71aa0b265c0f45a89eec7e4
Dalgona <dalgona@hontou.moe>
2016-10-18 12:12:03 +0900

    [WIP] refactor & improve readability

--- lib/serum/build/renderer.ex
+++ lib/serum/build/renderer.ex
@@ -4,23 +4,23 @@ defmodule Serum.Build.Renderer do
   @re_pages ~r/(?<type>href|src)="%25pages:(?<url>[^"]*)"/
 
   def process_links(text, proj) do
-    base = Keyword.get proj, :base_url
-    text = Regex.replace @re_media, text, ~s(\\1="#{base}media/\\2")
-    text = Regex.replace @re_posts, text, ~s(\\1="#{base}posts/\\2.html")
-    text = Regex.replace @re_pages, text, ~s(\\1="#{base}\\2.html")
+    base = Keyword.get(proj, :base_url)
+    text = Regex.replace(@re_media, text, ~s(\\1="#{base}media/\\2"))
+    text = Regex.replace(@re_posts, text, ~s(\\1="#{base}posts/\\2.html"))
+    text = Regex.replace(@re_pages, text, ~s(\\1="#{base}\\2.html"))
     text
   end
 
   def genpage(contents, ctx) do
-    proj = Serum.get_data :proj
-    base = Serum.get_data "template_base"
-    contents = process_links contents, proj
-    render base, proj ++ ctx ++ [contents: contents, navigation: Serum.get_data(:navstub)]
+    proj = Serum.get_data(:proj)
+    base = Serum.get_data("template_base")
+    contents = process_links(contents, proj)
+    render(base, proj ++ ctx ++ [contents: contents, navigation: Serum.get_data(:navstub)])
   end
 
   def render(template, assigns) do
-    proj = Serum.get_data :proj
-    {html, _} = Code.eval_quoted template, [assigns: proj ++ assigns]
+    proj = Serum.get_data(:proj)
+    {html, _} = Code.eval_quoted(template, [assigns: proj ++ assigns])
     html
   end
 end
