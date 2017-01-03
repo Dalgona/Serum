b491d360e7c078eef7962d59c9f9a4ae767d0aa8
Dalgona <dalgona@hontou.moe>
2017-01-04 02:20:27 +0900

    Add tests for `Serum.Build.Preparation.load_templates/1`

--- test/serum/preparation_test.exs
+++ test/serum/preparation_test.exs
@@ -30,13 +30,55 @@ defmodule Serum.PreparationTest do
 
     test "schema validation error" do
       path = priv_dir "test_projinfo/schema_error"
-      {:error, :child_task, {task, suberrors}} = load_info path
+      {:error, reason, {task, suberrors}} = load_info path
+      assert reason == :child_tasks
       assert task == :validate_json
       suberrors =
         suberrors
         |> Stream.map(fn {:error, reason, _} -> reason end)
         |> Enum.uniq
       assert suberrors == [:validation_error]
+    end
+  end
+
+  describe "load_templates/1" do
+    test "all ok" do
+      assert :ok = load_templates priv_dir("testsite_good")
+    end
+
+    # `list.html.eex` and `post.html.eex` are missing.
+    test "some are missing" do
+      path = priv_dir "test_templates/missing"
+      {:error, reason, {task, suberrors}} = load_templates path
+      assert reason == :child_tasks
+      assert task == :load_templates
+      assert 2 == length suberrors
+    end
+
+    # `base.html.eex` is malformed
+    test "eex syntax error" do
+      path = priv_dir "test_templates/eex_error"
+      {:error, reason, {task, [suberror]}} = load_templates path
+      assert reason == :child_tasks
+      assert task == :load_templates
+      {:error, reason2, {msg, _, _}} = suberror
+      assert reason2 == :invalid_template
+      assert msg == "missing token '%>'"
+    end
+
+    # `base.html.eex`: SyntaxError
+    # `nav.html.eex` : TokenMissingError
+    test "elixir syntax error" do
+      path = priv_dir "test_templates/elixir_error"
+      {:error, reason, {task, [sub1, sub2]}} = load_templates path
+      assert reason == :child_tasks
+      assert task == :load_templates
+      {:error, reason1, {msg1, _, _}} = sub1
+      assert reason1 == :invalid_template
+      assert msg1 == "syntax error before: '<'"
+      {:error, reason2, {msg2, _, _}} = sub2
+      assert reason2 == :invalid_template
+      assert msg2 == "missing terminator: \" (for string starting at line 1)"
     end
   end
 
