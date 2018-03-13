b31586d175020e06ef022939ebc27befe5e8a301
Dalgona <dalgona@hontou.moe>
2018-03-13 22:54:55 +0900

    [WIP] Modify PostList

--- lib/serum/post_list.ex
+++ lib/serum/post_list.ex
@@ -8,16 +8,28 @@ defmodule Serum.PostList do
 
   @type t :: %__MODULE__{
           tag: maybe_tag(),
-          page: pos_integer(),
+          current_page: pos_integer(),
           max_page: pos_integer(),
           title: binary(),
           posts: [Post.t()],
-          list_url: binary(),
+          url: binary(),
+          prev_url: binary() | nil,
+          next_url: binary() | nil,
           output: binary()
         }
   @type maybe_tag :: Tag.t() | nil
 
-  defstruct [:tag, :page, :max_page, :title, :posts, :list_url, :output]
+  defstruct [
+    :tag,
+    :current_page,
+    :max_page,
+    :title,
+    :posts,
+    :url,
+    :prev_url,
+    :next_url,
+    :output
+  ]
 
   @spec generate(maybe_tag(), [Post.t()], map()) :: [t()]
   def generate(tag, posts, proj) do
@@ -32,17 +44,36 @@ defmodule Serum.PostList do
     max_page = length(paginated_posts)
     list_dir = (tag && Path.join("tags", tag.name)) || "posts"
 
-    Enum.map(paginated_posts, fn {posts, page} ->
-      %__MODULE__{
-        tag: tag,
-        page: page,
-        max_page: max_page,
-        title: list_title(tag, proj),
-        posts: posts,
-        list_url: Path.join([proj.base_url, list_dir, "page-#{page}.html"]),
-        output: Path.join([proj.dest, list_dir, "page-#{page}.html"])
-      }
-    end)
+    lists =
+      Enum.map(paginated_posts, fn {posts, page} ->
+        %__MODULE__{
+          tag: tag,
+          current_page: page,
+          max_page: max_page,
+          title: list_title(tag, proj),
+          posts: posts,
+          url: Path.join([proj.base_url, list_dir, "page-#{page}.html"]),
+          output: Path.join([proj.dest, list_dir, "page-#{page}.html"])
+        }
+      end)
+
+    put_adjacent_urls([nil | lists], [])
+  end
+
+  @spec put_adjacent_urls([t()], [t()]) :: [t()]
+  defp put_adjacent_urls(lists, acc)
+  defp put_adjacent_urls([_last], acc), do: Enum.reverse(acc)
+
+  defp put_adjacent_urls([prev, curr | rest], acc) do
+    next = List.first(rest)
+
+    updated_curr = %__MODULE__{
+      curr
+      | prev_url: prev && prev.url,
+        next_url: next && next.url
+    }
+
+    put_adjacent_urls([curr | rest], [updated_curr | acc])
   end
 
   @spec make_chunks([Post.t()], boolean(), pos_integer()) :: [[Post.t()]]
@@ -53,15 +84,13 @@ defmodule Serum.PostList do
     Enum.chunk_every(posts, num_posts)
   end
 
-  @spec to_fragment([t()]) :: Result.t([Fragment.t()])
-  def to_fragment(post_lists) do
-    case to_html(post_lists) do
-      {:ok, htmls} ->
-        [first | rest] =
-          post_lists
-          |> Stream.zip(htmls)
-          |> Enum.map(fn {list, html} -> Fragment.new(:list, list, html) end)
-
+  @spec to_fragments([t()]) :: Result.t([Fragment.t()])
+  def to_fragments(post_lists) do
+    post_lists
+    |> Enum.map(&to_fragment/1)
+    |> Result.aggregate_values(:to_fragments)
+    |> case do
+      {:ok, [first | rest]} ->
         first_dup = %Fragment{
           first
           | output: first.output |> Path.dirname() |> Path.join("index.html")
@@ -74,35 +103,20 @@ defmodule Serum.PostList do
     end
   end
 
-  @spec to_html([t()]) :: Result.t(binary())
-  def to_html(post_lists) do
+  @spec to_fragment(t()) :: Result.t(Fragment.t())
+  defp to_fragment(post_list) do
+    metadata =
+      post_list
+      |> Map.drop([:__struct__, :output])
+      |> Map.put(:type, :list)
+
+    bindings = [page: metadata]
     template = Template.get("list")
-    render([nil | post_lists], [], template)
-  end
 
-  @spec render([t()], [Result.t(binary())], Template.t()) :: Result.t([binary()])
-
-  defp render([_last], acc, _template) do
-    acc
-    |> Enum.reverse()
-    |> Result.aggregate_values(:render)
-  end
-
-  defp render([prev, curr | rest], acc, template) do
-    next = List.first(rest)
-
-    bindings = [
-      header: curr.title,
-      posts: curr.posts,
-      current_page: curr.page,
-      max_page: curr.max_page,
-      prev_page: prev && prev.list_url,
-      next_page: next && next.list_url
-    ]
-
-    rendered = Renderer.render_fragment(template, bindings)
-
-    render([curr | rest], [rendered | acc], template)
+    case Renderer.render_fragment(template, bindings) do
+      {:ok, html} -> {:ok, Fragment.new(:list, post_list, html)}
+      {:error, _} = error -> error
+    end
   end
 
   @spec list_title(maybe_tag(), map()) :: binary()
