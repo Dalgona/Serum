d36c9d697d3b791c185d2c8ae798cd90ed311efc
Dalgona <dalgona@hontou.moe>
2019-01-01 00:53:20 +0900

    Merge Pass2.IndexBuilder to FragmentGenerator

--- lib/serum/build/fragment_generator.ex
+++ lib/serum/build/fragment_generator.ex
@@ -1,8 +1,8 @@
 defmodule Serum.Build.FragmentGenerator do
-  alias Serum.Build.Pass2.IndexBuilder
   alias Serum.Fragment
   alias Serum.Page
   alias Serum.Post
+  alias Serum.PostList
   alias Serum.ProjectInfo, as: Proj
   alias Serum.Result
 
@@ -13,7 +13,7 @@ defmodule Serum.Build.FragmentGenerator do
     tasks = [
       Task.async(fn -> task_fun(map.pages, Page, proj) end),
       Task.async(fn -> task_fun(map.posts, Post, proj) end),
-      Task.async(IndexBuilder, :run, [map.posts, map.tag_map, proj])
+      Task.async(fn -> list_task_fun(map.posts, map.tag_map, proj) end)
     ]
 
     tasks
@@ -31,5 +31,31 @@ defmodule Serum.Build.FragmentGenerator do
     |> Task.async_stream(struct, :to_fragment, [proj])
     |> Enum.map(&elem(&1, 1))
     |> Result.aggregate_values(:fragment_generator)
+  end
+
+  @spec list_task_fun([Post.t()], map(), Proj.t()) :: Result.t([Fragment.t()])
+  defp list_task_fun(posts, tag_map, proj) do
+    IO.puts("Generating post lists...")
+
+    all_posts = PostList.generate(nil, posts, proj)
+
+    tag_lists =
+      tag_map
+      |> Task.async_stream(fn {tag, posts} ->
+        PostList.generate(tag, posts, proj)
+      end)
+      |> Enum.map(&elem(&1, 1))
+
+    [all_posts | tag_lists]
+    |> Task.async_stream(PostList, :to_fragments, [])
+    |> Enum.map(&elem(&1, 1))
+    |> Result.aggregate_values(:fragment_generator)
+    |> case do
+      {:ok, fragments} ->
+        {:ok, List.flatten(fragments)}
+
+      {:error, _} = error ->
+        error
+    end
   end
 end
