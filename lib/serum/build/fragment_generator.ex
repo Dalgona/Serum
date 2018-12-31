dc50db6e6806172a2effcf22f4083fd8505c4de5
Dalgona <dalgona@hontou.moe>
2018-12-31 22:56:30 +0900

    Refactor FragmentGenerator, partly replace Pass2

--- lib/serum/build/fragment_generator.ex
+++ lib/serum/build/fragment_generator.ex
@@ -1,15 +1,18 @@
 defmodule Serum.Build.FragmentGenerator do
-  alias Serum.Build.Pass2.PageBuilder
-  alias Serum.Build.Pass2.PostBuilder
   alias Serum.Build.Pass2.IndexBuilder
   alias Serum.Fragment
+  alias Serum.Page
+  alias Serum.Post
+  alias Serum.ProjectInfo, as: Proj
   alias Serum.Result
 
   @spec to_fragment(map(), map()) :: Result.t([Fragment.t()])
   def to_fragment(map, proj) do
+    IO.puts("Generating fragments...")
+
     tasks = [
-      Task.async(PageBuilder, :run, [map.pages, proj]),
-      Task.async(PostBuilder, :run, [map.posts, proj]),
+      Task.async(fn -> task_fun(map.pages, Page, proj) end),
+      Task.async(fn -> task_fun(map.posts, Post, proj) end),
       Task.async(IndexBuilder, :run, [map.posts, map.tag_map, proj])
     ]
 
@@ -20,5 +23,13 @@ defmodule Serum.Build.FragmentGenerator do
       {:ok, results} -> {:ok, List.flatten(results)}
       {:error, _} = error -> error
     end
+  end
+
+  @spec task_fun([struct()], module(), Proj.t()) :: Result.t([Fragment.t()])
+  defp task_fun(items, struct, proj) do
+    items
+    |> Task.async_stream(struct, :to_fragment, [proj])
+    |> Enum.map(&elem(&1, 1))
+    |> Result.aggregate_values(:fragment_generator)
   end
 end
