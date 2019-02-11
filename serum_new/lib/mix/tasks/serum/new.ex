30cd3a1c95067985a4f3ec071add88667d6d4bea
Dalgona <dalgona@hontou.moe>
2019-02-11 18:54:17 +0900

    [WIP] Implement mix serum.new
    Implemented pre-condition checks.

--- serum_new/lib/mix/tasks/serum/new.ex
+++ serum_new/lib/mix/tasks/serum/new.ex
@@ -24,19 +24,96 @@ defmodule Mix.Tasks.Serum.New do
   @shortdoc "Creates a new Serum project"
 
   use Mix.Task
+  require Mix.Generator
+  alias Mix.Generator, as: MixGen
+
+  @options [
+    force: :boolean
+  ]
 
   @impl true
   def run(args) do
-    Mix.raise("not implemented. Argument(s): #{inspect(args)}")
+    {options, argv} = OptionParser.parse!(args, strict: @options)
+
+    case argv do
+      [] ->
+        Mix.raise("expected PATH to be given. Run mix help serum.new for help")
+
+      [path | _] ->
+        force? = options[:force] || false
+        :ok = check_path!(path, force?)
+        app_name = Path.basename(Path.expand(path))
+        :ok = check_app_name!(app_name)
+
+        assigns = [
+          app_name: app_name,
+          mod_name: Macro.camelize(app_name),
+          elixir_version: get_version!()
+        ]
+
+        if path != "." do
+          MixGen.create_directory(path)
+        end
+
+        File.cd!(path, fn -> generate_project(path, assigns) end)
+    end
 
     # Things to implement:
-    # - Parse command line arguments using OptionParser
-    # - Check if `args` contains PATH
-    # - Check if PATH already exists and is not empty (or --force is given)
-    # - Check if PATH matches the pattern ~r/[a-z][a-z0-9_]*/
     # - After all checks has passed,
     #   - Create directory structure
     #   - Create files
     #   - Print completion message
+  end
+
+  @spec check_path!(binary(), boolean()) :: :ok | no_return()
+  defp check_path!(path, force?)
+  defp check_path!(_path, true), do: :ok
+
+  defp check_path!(path, false) do
+    if File.exists?(path) do
+      case File.ls!(path) do
+        [] ->
+          :ok
+
+        [_ | _] ->
+          Mix.raise(
+            "#{path} already exists and is not empty. " <>
+              "Try again with a --force option to override"
+          )
+      end
+    else
+      :ok
+    end
+  end
+
+  @spec check_app_name!(binary()) :: :ok | no_return()
+  defp check_app_name!(app_name) do
+    if app_name =~ ~r/^[a-z][a-z0-9_]*$/ do
+      :ok
+    else
+      Mix.raise(
+        "PATH must start with a lowercase ASCII letter, " <>
+          "followed by zero or more lowercase ASCII letters, digits, " <>
+          "or underscores. Got: #{inspect(app_name)}"
+      )
+    end
+  end
+
+  @spec get_version!() :: binary()
+  defp get_version! do
+    ver = Version.parse!(System.version())
+
+    pre_release =
+      case ver.pre do
+        [] -> ""
+        [x | _xs] -> "-#{x}"
+      end
+
+    "#{ver.major}.#{ver.minor}#{pre_release}"
+  end
+
+  @spec generate_project(binary(), keyword()) :: :ok
+  defp generate_project(path, assigns) do
+    Mix.raise("not implemented\npath: #{inspect(path)}\nassigns: #{inspect(assigns)}")
   end
 end
