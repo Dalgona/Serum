cb9adf55e7b6bd84fe7c47dfe9c39bc4d63fa692
Dalgona <dalgona@hontou.moe>
2019-02-12 22:15:22 +0900

    Implement mix serum.gen.{page,post} tasks

---
+++ lib/mix/tasks/serum/gen.page.ex
@@ -0,0 +1,111 @@
+defmodule Mix.Tasks.Serum.Gen.Page do
+  @moduledoc """
+  Adds a new page to the current project.
+
+      mix serum.gen.page (-t|--title) TITLE (-o|--output) OUTPUT [Options]
+
+  ## Required Options
+
+  - `-t(--title)` (string): Title of the new page.
+  - `-o(--output)` (string): The path where the new page will be saved,
+    relative to `pages/` directory. It must end with one of `.md`, `.html`,
+    or `.html.eex`.
+
+  ## Other Options
+
+  - `-l(--label)` (string): Label of the new page. Defaults to the page title.
+  - `-g(--group)` (string): Name of a group the new page belongs to.
+  - `-r(--order)` (integer): The order of the new page in a group.
+    Defaults to `0`.
+  """
+
+  @shortdoc "Adds a new page to the current project"
+
+  use Mix.Task
+  alias Mix.Generator, as: MixGen
+  alias OptionParser.ParseError
+
+  @options [
+    strict: [
+      title: :string,
+      label: :string,
+      group: :string,
+      order: :integer,
+      output: :string
+    ],
+    aliases: [
+      t: :title,
+      l: :label,
+      g: :group,
+      r: :order,
+      o: :output
+    ]
+  ]
+
+  @required [:title, :output]
+  @in_header [:title, :label, :group, :order]
+
+  @impl true
+  def run(args) do
+    {options, argv} = OptionParser.parse!(args, @options)
+
+    if argv != [] do
+      raise ParseError, "\nExtra arguments: #{Enum.join(argv, ", ")}"
+    end
+
+    :ok = check_required!(options)
+    type = check_type!(options[:output])
+    output = Path.join("pages/", options[:output])
+    options2 = apply_default(options)
+
+    header =
+      options2
+      |> Enum.filter(fn {k, _v} -> k in @in_header end)
+      |> Enum.map(fn {k, v} -> "#{k}: #{v}" end)
+      |> Enum.join("\n")
+
+    title = options2[:title]
+
+    {title, body} =
+      case type do
+        :md ->
+          {"# #{title}", "TODO: Put some contents here!"}
+
+        _ ->
+          {"<h1>#{title}</h1>", "<p>TODO: Put some contents here!</p>"}
+      end
+
+    if not File.exists?("pages") do
+      MixGen.create_directory("pages")
+    end
+
+    MixGen.create_file(output, "---\n#{header}\n---\n\n#{title}\n\n#{body}\n")
+  end
+
+  defp check_required!(opts) do
+    case @required -- Keyword.keys(opts) do
+      [] -> :ok
+      [x | _] -> raise ParseError, "\nExpected \"--#{x}\" option to be given"
+    end
+  end
+
+  defp check_type!(path) do
+    cond do
+      String.ends_with?(path, ".md") ->
+        :md
+
+      String.ends_with?(path, ".html") ->
+        :html
+
+      String.ends_with?(path, ".html.eex") ->
+        :eex
+
+      :else ->
+        Mix.raise("Output file type must be one of .md, .html, or .html.eex")
+    end
+  end
+
+  defp apply_default(options) do
+    Keyword.merge([label: options[:title], order: 0], options)
+  end
+end
---
+++ lib/mix/tasks/serum/gen.post.ex
@@ -0,0 +1,101 @@
+defmodule Mix.Tasks.Serum.Gen.Post do
+  @moduledoc """
+  Adds a new blog post to the current project.
+
+      mix serum.gen.post (-t|--title) TITLE (-o|--output) OUTPUT [Options]
+
+  Post date will be automatically set to the moment this task is executed.
+
+  ## Required Options
+
+  - `-t(--title)` (string): Title of the new blog post.
+  - `-o(--output)` (string): Name of the generated post file. The actual path
+    to the generated file will be in the form of
+    `posts/YYYY-MM-DD-<OUTPUT>.md`.
+
+  ## Other Options
+
+  - `-g(--tag)` (string): Tag(s) of the new post. You can provide this option
+    zero or more times to give multiple tags to the post.
+  """
+
+  @shortdoc "Adds a new blog post to the current project"
+
+  use Mix.Task
+  alias Mix.Generator, as: MixGen
+  alias OptionParser.ParseError
+
+  @options [
+    strict: [
+      title: :string,
+      tag: :keep,
+      output: :string
+    ],
+    aliases: [
+      t: :title,
+      g: :tag,
+      o: :output
+    ]
+  ]
+
+  @required [:title, :output]
+
+  @impl true
+  def run(args) do
+    {options, argv} = OptionParser.parse!(args, @options)
+
+    if argv != [] do
+      raise ParseError, "\nExtra arguments: #{Enum.join(argv, ", ")}"
+    end
+
+    {:ok, _} = Application.ensure_all_started(:timex)
+
+    :ok = check_required!(options)
+    now = get_now!()
+    path = get_path(options[:output], now)
+
+    if not File.exists?("posts") do
+      MixGen.create_directory("posts")
+    end
+
+    tags =
+      case Keyword.get_values(options, :tag) do
+        [] -> ""
+        list -> Enum.join(list, ", ") <> "\n"
+      end
+
+    MixGen.create_file(
+      path,
+      """
+      ---
+      title: #{options[:title]}
+      date: #{Timex.format!(now, "{YYYY}-{0M}-{0D} {h24}:{m}:{s}")}
+      #{tags}---
+
+      TODO: Put some contents here!
+      """
+    )
+  end
+
+  defp check_required!(opts) do
+    case @required -- Keyword.keys(opts) do
+      [] -> :ok
+      [x | _] -> raise ParseError, "\nExpected \"--#{x}\" option to be given"
+    end
+  end
+
+  defp get_now! do
+    Timex.local()
+  rescue
+    _ -> Mix.raise("System timezone is not properly set")
+  end
+
+  defp get_path(output, now) do
+    date_part =
+      [now.year, now.month, now.day]
+      |> Enum.map(&(&1 |> to_string() |> String.pad_leading(2, "0")))
+      |> Enum.join("-")
+
+    Path.join("posts/", date_part <> "-" <> output <> ".md")
+  end
+end
