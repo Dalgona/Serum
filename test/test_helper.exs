81c75cfb58c87ec8e0ede1e72a5522fe7e9f9530
Dalgona <dalgona@hontou.moe>
2019-04-23 23:48:27 +0900

    Define Serum.TestHelper in test_helper.exs
    Add fixture/1 macro which helps get paths to test fixture files

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,9 @@
 ExUnit.start()
+
+defmodule Serum.TestHelper do
+  @priv_dir :serum |> :code.priv_dir() |> IO.iodata_to_binary()
+
+  defmacro fixture(arg) do
+    quote(do: Path.join([unquote(@priv_dir), "fixtures", unquote(arg)]))
+  end
+end
