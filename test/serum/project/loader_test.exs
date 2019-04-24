1b7728390b6043302c4b0cd4c1cf8f58c19eb1ef
Dalgona <dalgona@hontou.moe>
2019-04-24 16:30:45 +0900

    Add Serum.Project.LoaderTest

---
+++ test/serum/project/loader_test.exs
@@ -0,0 +1,48 @@
+defmodule Serum.Project.LoaderTest do
+  use ExUnit.Case, async: true
+  require Serum.TestHelper
+  import Serum.TestHelper, only: :macros
+  alias Serum.Project
+  alias Serum.Project.Loader, as: ProjectLoader
+
+  setup_all do
+    {:ok, [dest: "/path/to/dest/"]}
+  end
+
+  describe "load/2" do
+    test "load normal serum.exs", ctx do
+      src = fixture("proj/good/")
+      assert {:ok, %Project{}} = ProjectLoader.load(src, ctx.dest)
+    end
+
+    test "load non-existent file", ctx do
+      src = fixture("proj/non-existent")
+      file = Path.join(src, "serum.exs")
+      assert {:error, {:enoent, ^file, 0}} = ProjectLoader.load(src, ctx.dest)
+    end
+
+    test "load serum.json (unsupported)", ctx do
+      src = fixture("proj/bad-unsupported")
+      file = Path.join(src, "serum.json")
+      assert {:error, {_msg, ^file, 0}} = ProjectLoader.load(src, ctx.dest)
+    end
+
+    test "serum.exs compile-time error", ctx do
+      src = fixture("proj/bad-compile-error")
+      file = Path.join(src, "serum.exs")
+      assert {:error, {_msg, ^file, _line}} = ProjectLoader.load(src, ctx.dest)
+    end
+
+    test "serum.exs eval-time error", ctx do
+      src = fixture("proj/bad-eval-error")
+      file = Path.join(src, "serum.exs")
+      assert {:error, {_msg, ^file, _line}} = ProjectLoader.load(src, ctx.dest)
+    end
+
+    test "serum.exs validation error", ctx do
+      src = fixture("proj/bad-invalid")
+      assert {:error, {_, errors}} = ProjectLoader.load(src, ctx.dest)
+      assert length(errors) === 5
+    end
+  end
+end
