bac8f87b2bcd1dafbc79d69abf1e245aca8674a5
Dalgona <dalgona@hontou.moe>
2019-04-27 11:38:02 +0900

    FileEmitter: Replace mkdir_p!/1 with mkdir_p/1

--- lib/serum/build/file_emitter.ex
+++ lib/serum/build/file_emitter.ex
@@ -14,23 +14,38 @@ defmodule Serum.Build.FileEmitter do
   @spec run([Serum.File.t()]) :: Result.t()
   def run(files) do
     IO.puts("Writing output files...")
-    create_dirs(files)
+
+    case create_dirs(files) do
+      :ok ->
+        files
+        |> Enum.map(&write_file/1)
+        |> Result.aggregate(:file_emitter)
+
+      {:error, _} = error ->
+        error
+    end
 
     files
     |> Enum.map(&write_file/1)
     |> Result.aggregate(:file_emitter)
   end
 
-  @spec create_dirs([Serum.File.t()]) :: :ok
+  @spec create_dirs([Serum.File.t()]) :: Result.t()
   defp create_dirs(outputs) do
     outputs
     |> Stream.map(& &1.dest)
     |> Stream.map(&Path.dirname/1)
     |> Stream.uniq()
-    |> Enum.each(fn dir ->
-      File.mkdir_p!(dir)
-      IO.puts("\x1b[96m MKDIR \x1b[0m#{dir}")
-    end)
+    |> Enum.map(&create_dir/1)
+    |> Result.aggregate(:file_emitter)
+  end
+
+  @spec create_dir(binary()) :: Result.t()
+  defp create_dir(dir) do
+    case File.mkdir_p(dir) do
+      :ok -> IO.puts("\x1b[96m MKDIR \x1b[0m#{dir}")
+      {:error, reason} -> {:error, {reason, dir, 0}}
+    end
   end
 
   @spec write_file(Serum.File.t()) :: Result.t()
