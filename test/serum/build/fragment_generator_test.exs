81b52bfb78a9f6c920577a1e342d30f1f0583ad8
Dalgona <dalgona@hontou.moe>
2019-05-02 21:03:00 +0900

    Add Serum.Build.FragmentGeneratorTest

---
+++ test/serum/build/fragment_generator_test.exs
@@ -0,0 +1,62 @@
+defmodule Serum.Build.FragmentGeneratorTest do
+  use ExUnit.Case
+  require Serum.TestHelper
+  import Serum.TestHelper, only: :macros
+  alias Serum.Build.FragmentGenerator
+  alias Serum.GlobalBindings
+
+  setup_all do
+    {proj, _} = Code.eval_file(fixture("precompiled/good-proj.exs"))
+    {pages, _} = Code.eval_file(fixture("precompiled/good-pages.exs"))
+    {posts, _} = Code.eval_file(fixture("precompiled/good-posts.exs"))
+    {lists, _} = Code.eval_file(fixture("precompiled/good-lists.exs"))
+    {state, _} = Code.eval_file(fixture("precompiled/good-gb.exs"))
+
+    {:ok, [proj: proj, pages: pages, posts: posts, lists: lists, state: state]}
+  end
+
+  setup do
+    on_exit(fn ->
+      Agent.update(GlobalBindings, fn _ -> %{} end)
+    end)
+  end
+
+  describe "to_fragment/2" do
+    test "all went well", ctx do
+      proj = ctx.proj
+      {templates, _} = Code.eval_file(fixture("precompiled/good-templates.exs"))
+
+      GlobalBindings.load(ctx.state)
+
+      processed = %{
+        pages: ctx.pages,
+        posts: ctx.posts,
+        lists: ctx.lists,
+        templates: templates,
+        includes: []
+      }
+
+      {:ok, fragments} = mute_stdio(do: FragmentGenerator.to_fragment(processed, proj))
+      actual_count = length(ctx.pages) + length(ctx.posts) + length(ctx.lists)
+
+      assert length(fragments) === actual_count
+    end
+
+    test "failed due to bad templates", ctx do
+      proj = ctx.proj
+      {templates, _} = Code.eval_file(fixture("precompiled/bad-templates.exs"))
+
+      GlobalBindings.load(ctx.state)
+
+      processed = %{
+        pages: ctx.pages,
+        posts: ctx.posts,
+        lists: ctx.lists,
+        templates: templates,
+        includes: []
+      }
+
+      assert {:error, _} = mute_stdio(do: FragmentGenerator.to_fragment(processed, proj))
+    end
+  end
+end
