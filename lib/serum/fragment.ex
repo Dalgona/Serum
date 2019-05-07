9eb15e0e1285ee7ea36b5624633483f8a2ee5c9f
Dalgona <dalgona@hontou.moe>
2019-05-07 12:10:47 +0900

    Auto-set element id for heading tags, close #44

--- lib/serum/fragment.ex
+++ lib/serum/fragment.ex
@@ -10,6 +10,7 @@ defmodule Serum.Fragment do
   * `data`: Contents of the page fragment
   """
 
+  alias Serum.HtmlTreeHelper, as: Html
   alias Serum.Plugin
 
   @type t :: %__MODULE__{
@@ -24,18 +25,17 @@ defmodule Serum.Fragment do
   @doc "Creates a new `Fragment` struct."
   @spec new(binary() | nil, binary(), map(), binary()) :: Result.t(t())
   def new(file, output, metadata, data) do
-    case Plugin.rendering_fragment(Floki.parse(data), metadata) do
+    data
+    |> Floki.parse()
+    |> Html.traverse(%{}, &set_header_ids/2)
+    |> elem(0)
+    |> Plugin.rendering_fragment(metadata)
+    |> case do
       {:ok, html_tree} ->
-        images =
-          html_tree
-          |> Floki.find("img")
-          |> Enum.map(fn {"img", attrs, _} -> Map.new(attrs)["src"] || [] end)
-          |> List.flatten()
-
         fragment = %__MODULE__{
           file: file,
           output: output,
-          metadata: Map.put(metadata, :images, images),
+          metadata: Map.put(metadata, :images, extract_images(html_tree)),
           data: Floki.raw_html(html_tree)
         }
 
@@ -44,6 +44,52 @@ defmodule Serum.Fragment do
       {:error, _} = error ->
         error
     end
+  end
+
+  @spec set_header_ids(Html.tree(), map()) :: {Html.tree(), map()}
+  defp set_header_ids(tree, state)
+
+  defp set_header_ids({<<?h, ch::8>>, _, _} = tree, state) when ch in ?1..?6 do
+    {tag_name, attrs, children} = tree
+
+    case Enum.find(attrs, fn {k, _} -> k === "id" end) do
+      {"id", id} ->
+        {tree, increase_count(state, id)}
+
+      nil ->
+        id = generate_id(tree)
+
+        new_tree =
+          case state[id] do
+            nil -> {tag_name, [{"id", id} | attrs], children}
+            x -> {tag_name, [{"id", "#{id}-#{x + 1}"} | attrs], children}
+          end
+
+        {new_tree, increase_count(state, id)}
+    end
+  end
+
+  defp set_header_ids(tree, state), do: {tree, state}
+
+  @spec generate_id(Html.tree()) :: binary()
+  defp generate_id(tree) do
+    tree
+    |> Floki.text()
+    |> String.downcase()
+    |> String.replace(~r/\s/, "-")
+  end
+
+  @spec increase_count(map(), binary()) :: map()
+  defp increase_count(map, id), do: Map.update(map, id, 1, &(&1 + 1))
+
+  @spec extract_images(Html.tree()) :: [binary()]
+  defp extract_images(tree) do
+    tree
+    |> Floki.find("img")
+    |> Enum.map(fn {"img", attrs, _} ->
+      attrs |> Enum.find(fn {k, _} -> k === "src" end) |> elem(1)
+    end)
+    |> List.flatten()
   end
 
   defprotocol Source do
