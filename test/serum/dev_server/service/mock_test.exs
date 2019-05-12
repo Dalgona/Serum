952d98559885a58a84ceaa6d6ccc0a15deed9c7d
Dalgona <dalgona@hontou.moe>
2019-05-12 18:47:20 +0900

    Add Serum.DevServer.Service.Mock

---
+++ test/serum/dev_server/service/mock_test.exs
@@ -0,0 +1,39 @@
+defmodule Serum.DevServer.Service.MockTest do
+  use ExUnit.Case, async: true
+  alias Serum.DevServer.Service.Mock, as: MockService
+
+  setup_all do
+    child = %{
+      id: MockService,
+      start: {MockService, :start_link, ["/tmp/src/", "/tmp/site/"]}
+    }
+
+    start_supervised!(child)
+
+    :ok
+  end
+
+  test "rebuild/0" do
+    assert :ok === MockService.rebuild()
+  end
+
+  test "source_dir/0" do
+    assert "/tmp/src/" === MockService.source_dir()
+  end
+
+  test "site_dir/0" do
+    assert "/tmp/site/" === MockService.site_dir()
+  end
+
+  test "port/0" do
+    assert 8080 === MockService.port()
+  end
+
+  test "dirty?/0" do
+    assert false === MockService.dirty?()
+  end
+
+  test "subscribe/0" do
+    assert :ok === MockService.subscribe()
+  end
+end
