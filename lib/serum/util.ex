36c922d6e2b662eee8c2dc10cb53855a80c6db14
Dalgona <dalgona@hontou.moe>
2019-05-13 11:09:16 +0900

    Remove unused macros from Serum.Util

--- lib/serum/util.ex
+++ lib/serum/util.ex
@@ -6,34 +6,14 @@ defmodule Serum.Util do
 
   @doc "Prints a warning message to stderr."
   @spec warn(binary) :: Macro.t()
-
   defmacro warn(str) do
     quote do
       IO.puts(:stderr, "\x1b[33m * #{unquote(str)}\x1b[0m")
     end
   end
 
-  @doc "Displays which file is generated."
-  @spec msg_gen(binary) :: Macro.t()
-
-  defmacro msg_gen(dest) do
-    quote do
-      IO.puts("\x1b[92m  GEN  \x1b[0m#{unquote(dest)}")
-    end
-  end
-
-  @doc "Displays which file is generated."
-  @spec msg_gen(binary, binary) :: Macro.t()
-
-  defmacro msg_gen(src, dest) do
-    quote do
-      IO.puts("\x1b[92m  GEN  \x1b[0m#{unquote(src)} -> #{unquote(dest)}")
-    end
-  end
-
   @doc "Displays which directory is created."
   @spec msg_mkdir(binary) :: Macro.t()
-
   defmacro msg_mkdir(dir) do
     quote do
       IO.puts("\x1b[96m MKDIR \x1b[0m#{unquote(dir)}")
