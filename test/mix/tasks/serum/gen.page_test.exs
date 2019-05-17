8b0b10df2e13f8fb2a580eb32d1a88c226dd3e29
Dalgona <dalgona@hontou.moe>
2019-05-17 14:34:30 +0900

    Add Mix.Tasks.Serum.Gen.PageTest

---
+++ test/mix/tasks/serum/gen.page_test.exs
@@ -0,0 +1,83 @@
+defmodule Mix.Tasks.Serum.Gen.PageTest do
+  use ExUnit.Case
+  require Serum.TestHelper
+  import Serum.TestHelper, only: :macros
+  alias Mix.Tasks.Serum.Gen.Page, as: GenPage
+
+  setup do
+    uniq = Base.url_encode64(:crypto.strong_rand_bytes(6))
+    tmp_dir = Path.expand("serum_test_" <> uniq, System.tmp_dir!())
+
+    File.mkdir_p!(tmp_dir)
+    on_exit(fn -> File.rm_rf!(tmp_dir) end)
+
+    {:ok, tmp_dir: tmp_dir}
+  end
+
+  describe "mix serum.gen.page" do
+    test "works well with required options only", %{tmp_dir: tmp_dir} do
+      File.cd!(tmp_dir, fn ->
+        mute_stdio do
+          GenPage.run(~w(-t Hello -o hello.md))
+          GenPage.run(~w(-t Hello -o hello.html.eex))
+        end
+
+        data = File.read!(Path.join([tmp_dir, "pages", "hello.md"]))
+
+        [
+          "title: Hello",
+          "order: 0"
+        ]
+        |> Enum.each(&assert String.contains?(data, &1))
+      end)
+    end
+
+    test "works well with all options", %{tmp_dir: tmp_dir} do
+      File.cd!(tmp_dir, fn ->
+        mute_stdio do
+          GenPage.run(~w(-t Hello -o hello.html -l wow -g test -r 3))
+        end
+
+        data = File.read!(Path.join([tmp_dir, "pages", "hello.html"]))
+
+        [
+          "title: Hello",
+          "group: test",
+          "order: 3",
+          "label: wow"
+        ]
+        |> Enum.each(&assert String.contains?(data, &1))
+      end)
+    end
+
+    test "fails on option type mismatch" do
+      assert_raise OptionParser.ParseError, fn ->
+        GenPage.run(~w(-t Hello -o hello.html -r foo))
+      end
+    end
+
+    test "fails if required options are missing" do
+      assert_raise OptionParser.ParseError, fn ->
+        GenPage.run(~w(-t Hello))
+      end
+    end
+
+    test "fails if there are extra arguments" do
+      assert_raise OptionParser.ParseError, fn ->
+        GenPage.run(~w(-t Hello -o hello.html foo bar baz))
+      end
+    end
+
+    test "fails if there are undefined options" do
+      assert_raise OptionParser.ParseError, fn ->
+        GenPage.run(~w(-t Hello -o hello.html -x -y --xyzzy))
+      end
+    end
+
+    test "fails if the file type is invalid" do
+      assert_raise Mix.Error, fn ->
+        GenPage.run(~w(-t Hello -o hello.txt))
+      end
+    end
+  end
+end
