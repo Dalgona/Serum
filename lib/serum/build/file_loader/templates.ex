fc95d1c5fe79524c35688d154d65db4f9a27b3d3
Dalgona <dalgona@hontou.moe>
2019-08-03 23:56:07 +0900

    Update Serum.Build.FileLoader.Templates

--- lib/serum/build/file_loader/templates.ex
+++ lib/serum/build/file_loader/templates.ex
@@ -11,35 +11,71 @@ defmodule Serum.Build.FileLoader.Templates do
   alias Serum.Result
   alias Serum.Theme
 
+  @required_templates MapSet.new(~w(base list page post))
+
   @doc false
   @spec load(binary()) :: Result.t([Serum.File.t()])
   def load(src) do
     put_msg(:info, "Loading template files...")
 
-    case Theme.get_templates() do
-      {:ok, paths} ->
-        paths
-        |> Map.merge(get_project_templates(src), fn _, v1, v2 ->
-          (File.exists?(v2) && v2) || v1
-        end)
-        |> Enum.map(&elem(&1, 1))
-        |> Plugin.reading_templates()
-        |> case do
-          {:ok, files} -> read_files(files)
-          {:error, _} = plugin_error -> plugin_error
-        end
-
+    with {:ok, theme_paths} <- Theme.get_templates(),
+         {:ok, proj_paths} <- get_project_templates(src),
+         merged <- Map.merge(theme_paths, proj_paths, &merge_fun/3),
+         :ok <- validate_required(merged, src) do
+      merged
+      |> Enum.map(&elem(&1, 1))
+      |> Plugin.reading_templates()
+      |> case do
+        {:ok, files} -> read_files(files)
+        {:error, _} = plugin_error -> plugin_error
+      end
+    else
       {:error, _} = error ->
         error
     end
   end
 
-  @spec get_project_templates(binary()) :: map()
+  @spec get_project_templates(binary()) :: Result.t(map())
   defp get_project_templates(src) do
     templates_dir = get_subdir(src, "templates")
 
-    ["base", "list", "page", "post"]
-    |> Enum.map(&{&1, Path.join(templates_dir, &1 <> ".html.eex")})
-    |> Map.new()
+    if File.exists?(templates_dir) do
+      map =
+        templates_dir
+        |> Path.join("*.html.eex")
+        |> Path.wildcard()
+        |> Enum.map(&{Path.basename(&1, ".html.eex"), &1})
+        |> Map.new()
+
+      {:ok, map}
+    else
+      {:error, {:enoent, templates_dir, 0}}
+    end
+  end
+
+  @spec merge_fun(binary(), binary(), binary()) :: binary()
+  defp merge_fun(_k, from_theme, from_proj) do
+    (File.exists?(from_proj) && from_proj) || from_theme
+  end
+
+  @spec validate_required(map(), binary()) :: Result.t()
+  defp validate_required(map, src) do
+    existing_templates = map |> Map.keys() |> MapSet.new()
+
+    @required_templates
+    |> MapSet.difference(existing_templates)
+    |> MapSet.to_list()
+    |> case do
+      [] ->
+        :ok
+
+      missings when is_list(missings) ->
+        errors =
+          Enum.map(missings, fn missing ->
+            {:error, {:enoent, Path.join([src, "templates", missing]), 0}}
+          end)
+
+        {:error, {:file_loader, errors}}
+    end
   end
 end
