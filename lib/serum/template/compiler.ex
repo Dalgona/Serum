69d8ec03f20b63a941eec06f23656ac65674c22a
Dalgona <dalgona@hontou.moe>
2019-08-18 11:29:03 +0900

    Show an awesome message when a cycle is detected

--- lib/serum/template/compiler.ex
+++ lib/serum/template/compiler.ex
@@ -119,13 +119,62 @@ defmodule Serum.Template.Compiler do
         raise ArgumentError, arg
 
       include ->
-        if arg in stack do
-          raise "cycle detected while expanding includes"
-        end
+        check_cycle!(arg, stack)
 
         {quote(do: (fn -> unquote(include.ast) end).()), [arg | stack]}
     end
   end
 
   defp do_expand_includes(anything_else, stack, _), do: {anything_else, stack}
+
+  top = "    \x1b[31m\u256d\u2500\u2500\u2500\u2500\u2500\u256e\x1b[m\n"
+  arrow = "    \x1b[31m\u2502     \u2193\x1b[m\n"
+  first_text = "    \x1b[31m\u2502    \x1b[33m"
+  rest_text = "    \x1b[31m\u2502    "
+  bottom1 = "    \x1b[31m\u2502     \u2502\x1b[m\n"
+  bottom2 = "    \x1b[31m\u2570\u2500\u2500\u2500\u2500\u2500\u256f\x1b[m\n"
+
+  @spec check_cycle!(binary(), [binary()]) :: nil | no_return()
+  defp check_cycle!(name, stack) do
+    if name in stack do
+      graph =
+        stack
+        |> Enum.reverse()
+        |> Enum.drop_while(&(&1 != name))
+        |> make_graph()
+
+      message = [
+        "cycle detected while expanding includes:\n",
+        graph,
+        "  Recursively including templates is not supported by Serum.\n",
+        "  Please refactor your templates to break the cycle."
+      ]
+
+      raise IO.iodata_to_binary(message)
+    end
+  end
+
+  @spec make_graph([binary()]) :: iodata()
+  defp make_graph([first | rest]) do
+    rest_graph =
+      Enum.map(rest, fn name ->
+        [
+          unquote(arrow),
+          unquote(rest_text),
+          name,
+          "\x1b[m\n"
+        ]
+      end)
+
+    [
+      unquote(top),
+      unquote(arrow),
+      unquote(first_text),
+      first,
+      "\x1b[m\n",
+      rest_graph,
+      unquote(bottom1),
+      unquote(bottom2)
+    ]
+  end
 end
