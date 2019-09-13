675d4ece750ab01c5bd6564907bd9fc78045ed14
Dalgona <dalgona@hontou.moe>
2019-09-13 21:03:56 +0900

    [BROKEN] Update Serum.Build.FileProcessor.Template
    - Use Serum.Template.Storage to store compile templates/includes.

--- lib/serum/build/file_processor/template.ex
+++ lib/serum/build/file_processor/template.ex
@@ -3,19 +3,38 @@ defmodule Serum.Build.FileProcessor.Template do
 
   import Serum.IOProxy, only: [put_msg: 2]
   alias Serum.Result
+  alias Serum.Template
   alias Serum.Template.Compiler, as: TC
+  alias Serum.Template.Storage, as: TS
 
-  @doc false
+  @type compile_result :: %{optional(binary()) => Template.t()}
+  @typep type :: Template.template_type()
+
+  # TODO: The return type will be `Result.t()`.
   @spec compile_templates(map()) :: Result.t({map(), map()})
-  def compile_templates(%{templates: templates, includes: includes}) do
-    put_msg(:info, "Compiling templates...")
+  def compile_templates(%{templates: template_files, includes: include_files}) do
+    put_msg(:info, "Compiling and loading templates...")
 
-    with {:ok, includes} <- TC.compile_files(includes, type: :include),
-         tc_options = [type: :template, includes: includes],
-         {:ok, templates} <- TC.compile_files(templates, tc_options) do
+    with {:ok, includes} <- compile_and_load(include_files, :include),
+         {:ok, templates} <- compile_and_load(template_files, :template) do
       {:ok, {templates, includes}}
     else
       {:error, _} = error -> error
+    end
+  end
+
+  # TODO: The return type will be `Result.t()`.
+  @spec compile_and_load([Serum.File.t()], type()) :: Result.t(compile_result())
+  defp compile_and_load(files, type) do
+    case TC.compile_files(files, type: type) do
+      {:ok, result} ->
+        TS.load(result, type)
+
+        # TODO: Remove this line after finishing the implementation.
+        {:ok, result}
+
+      {:error, _} = error ->
+        error
     end
   end
 end
