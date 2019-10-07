434c32de6e1832d262e71f54a73a3cbc30a03f1d
Dalgona <dalgona@hontou.moe>
2019-10-07 23:30:06 +0900

    Add Serum.Plugin.Macros module

---
+++ lib/serum/plugin/macros.ex
@@ -0,0 +1,14 @@
+defmodule Serum.Plugin.Macros do
+  @moduledoc false
+
+  _moduledocp = "Provides macros for defining the `Serum.Plugin` module."
+
+  defmacro defcallback({:"::", _, [{name, meta, args} = call, return]}) do
+    arg_expr = quote(do: args :: term())
+
+    quote do
+      @callback unquote(call) :: unquote(return)
+      @callback unquote({name, meta, args ++ [arg_expr]}) :: unquote(return)
+    end
+  end
+end
