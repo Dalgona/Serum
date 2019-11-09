659359f62d1604adf465ca359f019d0cf514b1fb
Dalgona <dalgona@hontou.moe>
2019-11-09 10:40:06 +0900

    Add `:include_resolved?` key to Template struct

--- lib/serum/template.ex
+++ lib/serum/template.ex
@@ -4,20 +4,22 @@ defmodule Serum.Template do
   @type t() :: %__MODULE__{
           type: type(),
           file: binary(),
-          ast: Macro.t()
+          ast: Macro.t(),
+          include_resolved?: boolean()
         }
 
   @type collection() :: %{optional(binary()) => t()}
   @type type() :: :template | :include
 
-  defstruct type: :template, file: nil, ast: nil
+  defstruct type: :template, file: nil, ast: nil, include_resolved?: false
 
   @spec new(Macro.t(), type(), binary()) :: t()
   def new(ast, type, path) do
     %__MODULE__{
       type: type,
       file: path,
-      ast: ast
+      ast: ast,
+      include_resolved?: false
     }
   end
 end
