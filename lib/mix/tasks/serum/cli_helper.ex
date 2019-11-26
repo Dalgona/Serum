e6c894dd83e7a966258cfd34dc173925d09e0687
Dalgona <dalgona@hontou.moe>
2019-11-26 14:46:34 +0900

    Add CLIHelper.parse_options/2
    - Common function used by all Mix tasks.
    - Processes common command line option: :color

--- lib/mix/tasks/serum/cli_helper.ex
+++ lib/mix/tasks/serum/cli_helper.ex
@@ -15,4 +15,26 @@ defmodule Mix.Tasks.Serum.CLIHelper do
     |> IO.ANSI.format()
     |> IO.iodata_to_binary()
   end
+
+  @spec parse_options([binary()], OptionParser.options()) :: keyword() | no_return()
+  def parse_options(args, options) do
+    options = put_in(options[:strict][:color], :boolean)
+    {options, argv} = OptionParser.parse!(args, options)
+    :ok = set_ansi(options[:color])
+
+    if argv != [] do
+      extras = Enum.join(argv, ", ")
+
+      raise OptionParser.ParseError, "\nExtra arguments: #{extras}"
+    end
+
+    options
+  end
+
+  defp set_ansi(value)
+  defp set_ansi(nil), do: :ok
+
+  defp set_ansi(value) when is_boolean(value) do
+    Application.put_env(:elixir, :ansi_enabled, value)
+  end
 end
