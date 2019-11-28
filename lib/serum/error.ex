db3ed370df2e3cd72260776cdbf73d450867a9d4
Dalgona <dalgona@hontou.moe>
2019-11-28 15:34:10 +0900

    Define Serum.Error struct

---
+++ lib/serum/error.ex
@@ -0,0 +1,16 @@
+defmodule Serum.Error do
+  @moduledoc "Defines a struct describing error information."
+
+  defstruct [:message, :exception, :stacktrace, :caused_by, :file, :line]
+
+  @type t :: %__MODULE__{
+          message: binary(),
+          exception: Exception.t() | nil,
+          stacktrace: stacktrace(),
+          caused_by: [t()],
+          file: Serum.File.t() | nil,
+          line: integer()
+        }
+
+  @typep stacktrace :: [{atom(), atom(), integer(), keyword()}]
+end
