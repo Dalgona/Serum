e4e4fed413a015f7289e55da989becafc4557cd5
Dalgona <dalgona@hontou.moe>
2019-11-29 23:45:51 +0900

    Copy call_action/2 and call_function/2

--- lib/serum/plugin/client.ex
+++ lib/serum/plugin/client.ex
@@ -8,10 +8,11 @@ defmodule Serum.Plugin.Client do
   import Serum.Plugin.Client.Macros
   alias Serum.Fragment
   alias Serum.Page
+  alias Serum.Plugin
   alias Serum.Post
   alias Serum.PostList
-  alias Serum.Template
   alias Serum.Result
+  alias Serum.Template
 
   interface :action, build_started(src :: binary(), dest :: binary()) :: Result.t({})
   interface :function, reading_pages(files :: [binary()]) :: Result.t([binary()])
@@ -43,12 +44,80 @@ defmodule Serum.Plugin.Client do
   interface :action, finalizing(src :: binary(), dest :: binary()) :: Result.t({})
 
   @spec call_action(atom(), [term()]) :: Result.t({})
-  defp call_action(name, args) do
-    raise "not implemented"
+  defp call_action(fun, args) do
+    Plugin
+    |> Agent.get(&(&1[fun] || []))
+    |> do_call_action(fun, args)
+  end
+
+  @spec do_call_action([{integer(), Plugin.t()}], atom(), [term()]) :: Result.t({})
+  defp do_call_action(arity_and_plugins, fun, args)
+  defp do_call_action([], _fun, _args), do: {:ok, {}}
+
+  defp do_call_action([{_arity, plugin} | arity_and_plugins], fun, args) do
+    new_args = args ++ [plugin.args]
+
+    case apply(plugin.module, fun, new_args) do
+      {:ok, _} ->
+        do_call_action(arity_and_plugins, fun, args)
+
+      {:error, _} = error ->
+        error
+
+      term ->
+        message =
+          "#{module_name(plugin.module)}.#{fun} returned " <>
+            "an unexpected value: #{inspect(term)}"
+
+        {:error, message}
+    end
+  rescue
+    exception -> handle_exception(exception, plugin.module, fun)
   end
 
   @spec call_function(atom(), [term()]) :: Result.t({})
-  def call_function(name, args) do
-    raise "not implemented"
+  def call_function(fun, [arg | args]) do
+    Plugin
+    |> Agent.get(&(&1[fun] || []))
+    |> do_call_function(fun, args, arg)
+  end
+
+  @spec do_call_function([{integer, Plugin.t()}], atom(), [term()], term()) :: Result.t(term())
+  defp do_call_function(arity_and_plugins, fun, args, acc)
+  defp do_call_function([], _fun, _args, acc), do: {:ok, acc}
+
+  defp do_call_function([{_arity, plugin} | arity_and_plugins], fun, args, acc) do
+    new_args = [acc | args] ++ [plugin.args]
+
+    case apply(plugin.module, fun, new_args) do
+      {:ok, new_acc} ->
+        do_call_function(arity_and_plugins, fun, args, new_acc)
+
+      {:error, _} = error ->
+        error
+
+      term ->
+        message =
+          "#{module_name(plugin.module)}.#{fun} returned " <>
+            "an unexpected value: #{inspect(term)}"
+
+        {:error, message}
+    end
+  rescue
+    exception -> handle_exception(exception, plugin.module, fun)
+  end
+
+  @spec handle_exception(Exception.t(), atom(), atom()) :: Result.t({})
+  defp handle_exception(exception, module, fun) do
+    ex_name = module_name(exception.__struct__)
+    ex_msg = Exception.message(exception)
+    msg = "#{ex_name} at #{module_name(module)}.#{fun}: #{ex_msg}"
+
+    {:error, msg}
+  end
+
+  @spec module_name(atom()) :: binary()
+  defp module_name(module) do
+    module |> to_string() |> String.replace_prefix("Elixir.", "")
   end
 end
