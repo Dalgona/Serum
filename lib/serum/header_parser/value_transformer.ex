2ddffd277ee59257e562fdb896697622719b2128
Dalgona <dalgona@hontou.moe>
2019-12-09 21:20:20 +0900

    Change typespec of ValueTransformer.transform_value
    Add optional argument: the line number information

--- lib/serum/header_parser/value_transformer.ex
+++ lib/serum/header_parser/value_transformer.ex
@@ -6,6 +6,7 @@ defmodule Serum.HeaderParser.ValueTransformer do
   """
 
   require Serum.Result, as: Result
+  alias Serum.Error
   alias Serum.HeaderParser
 
   @typep kv :: {binary(), binary()}
@@ -14,21 +15,21 @@ defmodule Serum.HeaderParser.ValueTransformer do
   @date_format1 "{YYYY}-{0M}-{0D} {h24}:{m}:{s}"
   @date_format2 "{YYYY}-{0M}-{0D}"
 
-  @spec transform_value(kv(), type()) :: Result.t(HeaderParser.value())
-  def transform_value(kv, type)
+  @spec transform_value(kv(), type(), integer()) :: Result.t(HeaderParser.value())
+  def transform_value(kv, type, line \\ 0)
 
-  def transform_value({_key, valstr}, :string) do
+  def transform_value({_key, valstr}, :string, _line) do
     Result.return(valstr)
   end
 
-  def transform_value({key, valstr}, :integer) do
+  def transform_value({key, valstr}, :integer, line) do
     case Integer.parse(valstr) do
       {value, ""} -> Result.return(value)
-      _ -> Result.fail(Simple, ["#{key}: invalid integer"])
+      _ -> Result.fail(Simple, ["#{key}: invalid integer"], line: line)
     end
   end
 
-  def transform_value({key, valstr}, :datetime) do
+  def transform_value({key, valstr}, :datetime, line) do
     case Timex.parse(valstr, @date_format1) do
       {:ok, dt} ->
         Result.return(local_datetime(dt))
@@ -36,26 +37,30 @@ defmodule Serum.HeaderParser.ValueTransformer do
       {:error, _msg} ->
         case Timex.parse(valstr, @date_format2) do
           {:ok, dt} -> Result.return(local_datetime(dt))
-          {:error, msg} -> Result.fail(Simple, ["#{key}: " <> msg])
+          {:error, msg} -> Result.fail(Simple, ["#{key}: " <> msg], line: line)
         end
     end
   end
 
-  def transform_value({key, _valstr}, {:list, {:list, _type}}) do
-    Result.fail(Simple, ["#{key}: \"list of lists\" type is not supported"])
+  def transform_value({key, _valstr}, {:list, {:list, _type}}, line) do
+    Result.fail(Simple, ["#{key}: \"list of lists\" type is not supported"], line: line)
   end
 
-  def transform_value({key, valstr}, {:list, type}) when is_atom(type) do
+  def transform_value({key, valstr}, {:list, type}, line) when is_atom(type) do
     valstr
     |> String.split(",")
     |> Stream.map(&String.trim/1)
     |> Stream.reject(&(&1 == ""))
     |> Enum.map(&transform_value({key, &1}, type))
     |> Result.aggregate("#{key}: failed to parse list item(s):")
+    |> case do
+      {:ok, _} = result -> result
+      {:error, %Error{} = error} -> {:error, %Error{error | line: line}}
+    end
   end
 
-  def transform_value({key, _valstr}, _type) do
-    Result.fail(Simple, ["#{key}: invalid value type"])
+  def transform_value({key, _valstr}, _type, line) do
+    Result.fail(Simple, ["#{key}: invalid value type"], line: line)
   end
 
   @spec local_datetime(DateTime.t() | NaiveDateTime.t()) :: DateTime.t()
