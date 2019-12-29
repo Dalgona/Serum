4cafe8255d32861164ae4333f381febf9f259fc9
Dalgona <dalgona@hontou.moe>
2019-12-29 21:15:52 +0900

    Show source line info if file and line are provided

--- lib/serum/error.ex
+++ lib/serum/error.ex
@@ -32,9 +32,16 @@ defmodule Serum.Error do
   end
 
   defimpl Format do
+    @type line :: {binary(), integer()}
+
     def format_text(error, indent) do
-      file_text = format_file_text(error.file, error.line)
-      head = indented([file_text, Format.format_text(error.message, 0)], indent)
+      contents = [
+        format_file_text(error.file, error.line),
+        Format.format_text(error.message, 0),
+        format_source_lines(error.file, error.line)
+      ]
+
+      head = indented(contents, indent)
       children = Enum.map(error.caused_by, &Format.format_text(&1, indent + 1))
 
       Enum.intersperse([head | children], ?\n)
@@ -49,6 +56,72 @@ defmodule Serum.Error do
         "" -> ""
         str when is_binary(str) -> [str, ?\s]
       end
+    end
+
+    @spec format_source_lines(Serum.File.t(), integer() | nil) :: IO.ANSI.ansidata()
+    defp format_source_lines(file, line)
+    defp format_source_lines(nil, _line), do: ""
+    defp format_source_lines(%Serum.File{src: nil}, _line), do: ""
+    defp format_source_lines(%Serum.File{in_data: nil}, _line), do: ""
+    defp format_source_lines(_file, nil), do: ""
+    defp format_source_lines(_file, line) when line < 1, do: ""
+
+    defp format_source_lines(%Serum.File{in_data: in_data}, line) do
+      {prev, current, next} = extract_lines(in_data, line)
+
+      gutter_width =
+        [current | next]
+        |> Enum.map(&elem(&1, 1))
+        |> Enum.max()
+        |> to_string()
+        |> String.length()
+
+      [
+        ?\n,
+        format_other_lines(prev, gutter_width),
+        format_current_line(current, gutter_width),
+        format_other_lines(next, gutter_width)
+      ]
+    end
+
+    @spec extract_lines(binary(), integer()) :: {[line()], line(), [line()]}
+    defp extract_lines(data, line) do
+      {:ok, string_io} = StringIO.open(data)
+      stream = IO.stream(string_io, :line)
+
+      [current | prev] =
+        stream
+        |> Stream.with_index(1)
+        |> Stream.take(line)
+        |> Enum.reverse()
+        |> Enum.take(4)
+
+      next = stream |> Stream.with_index(line + 1) |> Enum.take(3)
+      {:ok, _} = StringIO.close(string_io)
+
+      {Enum.reverse(prev), current, next}
+    end
+
+    @spec format_current_line(line(), integer()) :: IO.ANSI.ansidata()
+    defp format_current_line({str, line}, gutter_width) do
+      [
+        [:bright, :yellow],
+        String.pad_leading(to_string(line), gutter_width),
+        [:normal, :light_black, " | ", :bright, :yellow],
+        [str, :reset]
+      ]
+    end
+
+    @spec format_other_lines([line()], integer()) :: IO.ANSI.ansidata()
+    defp format_other_lines(lines, gutter_width) do
+      Enum.map(lines, fn {str, line} ->
+        [
+          :light_black,
+          String.pad_leading(to_string(line), gutter_width),
+          [" | ", :reset],
+          [str, :reset]
+        ]
+      end)
     end
 
     @spec indented(IO.ANSI.ansidata(), non_neg_integer()) :: IO.ANSI.ansidata()
