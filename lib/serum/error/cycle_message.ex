90b9220a54b2a079f166107cc8d6e65a657fdfbe
Dalgona <dalgona@hontou.moe>
2020-01-01 13:34:40 +0900

    Fix indentation of formatted text of CycleMessage

--- lib/serum/error/cycle_message.ex
+++ lib/serum/error/cycle_message.ex
@@ -19,19 +19,19 @@ defmodule Serum.Error.CycleMessage do
       [
         "cycle detected while expanding includes:\n",
         make_graph(cycle),
-        "  Cycles are not allowed when recursively including templates.\n",
-        "  Please refactor your templates to break the cycle.\n",
-        "  Alternatively, you can use the render/1,2 template helper.\n"
+        "Cycles are not allowed when recursively including templates.\n",
+        "Please refactor your templates to break the cycle.\n",
+        "Alternatively, you can use the render/1,2 template helper.\n"
       ]
     end
 
     endl = [:reset, ?\n]
-    top = ["    ", :red, "\u256d\u2500\u2500\u2500\u2500\u2500\u256e", endl]
-    arrow = ["    ", :red, "\u2502     \u2193", endl]
-    first_text = ["    ", :red, "\u2502    ", :yellow]
-    rest_text = ["    ", :red, "\u2502    "]
-    bottom1 = ["    ", :red, "\u2502     \u2502", endl]
-    bottom2 = ["    ", :red, "\u2570\u2500\u2500\u2500\u2500\u2500\u256f", endl]
+    top = ["  ", :red, "\u256d\u2500\u2500\u2500\u2500\u2500\u256e", endl]
+    arrow = ["  ", :red, "\u2502     \u2193", endl]
+    first_text = ["  ", :red, "\u2502    ", :yellow]
+    rest_text = ["  ", :red, "\u2502    "]
+    bottom1 = ["  ", :red, "\u2502     \u2502", endl]
+    bottom2 = ["  ", :red, "\u2570\u2500\u2500\u2500\u2500\u2500\u256f", endl]
 
     @spec make_graph([String.Chars.t()]) :: IO.ANSI.ansidata()
     defp make_graph([first | rest]) do
