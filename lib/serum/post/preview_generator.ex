47848d58762e655609bb0d89a64bf62d7ca71e33
Dalgona <dalgona@hontou.moe>
2020-02-20 09:39:42 +0900

    refactor(serum): remove deprecation warnings in PreviewGenerator

--- lib/serum/post/preview_generator.ex
+++ lib/serum/post/preview_generator.ex
@@ -9,20 +9,20 @@ defmodule Serum.Post.PreviewGenerator do
   def generate_preview(_html, {_, l}) when is_integer(l) and l <= 0, do: ""
 
   def generate_preview(html, l) when is_integer(l) do
-    do_generate_preview(html, {:chars, l})
+    html |> Floki.parse_document!() |> do_generate_preview({:chars, l})
   end
 
   def generate_preview(html, {_, l} = lspec) when is_integer(l) do
-    do_generate_preview(html, lspec)
+    html |> Floki.parse_document!() |> do_generate_preview(lspec)
   end
 
   def generate_preview(_html, _), do: ""
 
-  @spec do_generate_preview(binary(), {term(), non_neg_integer()}) :: binary()
+  @spec do_generate_preview(Floki.html_tree(), {term(), non_neg_integer()}) :: binary()
   defp do_generate_preview(html, lspec)
 
-  defp do_generate_preview(html, {:chars, l}) do
-    html
+  defp do_generate_preview(html_tree, {:chars, l}) do
+    html_tree
     |> Floki.text(sep: " ")
     |> String.trim()
     |> String.replace(~r/\s+/, " ")
@@ -30,8 +30,8 @@ defmodule Serum.Post.PreviewGenerator do
     |> Kernel.<>("\u2026")
   end
 
-  defp do_generate_preview(html, {:words, l}) do
-    html
+  defp do_generate_preview(html_tree, {:words, l}) do
+    html_tree
     |> Floki.text(sep: " ")
     |> String.split(~r/\s/, trim: true)
     |> Enum.take(l)
@@ -39,8 +39,8 @@ defmodule Serum.Post.PreviewGenerator do
     |> Kernel.<>("\u2026")
   end
 
-  defp do_generate_preview(html, {:paragraphs, l}) do
-    html
+  defp do_generate_preview(html_tree, {:paragraphs, l}) do
+    html_tree
     |> Floki.find("p")
     |> Enum.take(l)
     |> Enum.map(&(&1 |> Floki.text() |> String.trim()))
@@ -48,5 +48,5 @@ defmodule Serum.Post.PreviewGenerator do
     |> Kernel.<>("\u2026")
   end
 
-  defp do_generate_preview(_html, _), do: ""
+  defp do_generate_preview(_html_tree, _), do: ""
 end
