1e2b7b6882df585854c3b5da60dfbb771715ea38
Dalgona <dalgona@hontou.moe>
2020-05-03 19:31:57 +0900

    test(serum): update tests for Serum.Build.FileLoader.Includes

--- test/serum/build/file_loader/includes_test.exs
+++ test/serum/build/file_loader/includes_test.exs
@@ -1,110 +1,115 @@
 defmodule Serum.Build.FileLoader.IncludesTest do
   use Serum.Case
-  import Serum.Build.FileLoader.Includes
+  alias Serum.Build.FileLoader.Includes, as: IncludeLoader
   alias Serum.Plugin
+  alias Serum.Plugin.Loader, as: PluginLoader
   alias Serum.Theme
+  alias Serum.Theme.Loader, as: ThemeLoader
+  alias Serum.V2.Error
 
-  [
-    "theme_modules/real_dummy_theme.ex",
-    "theme_modules/failing_theme.ex"
-  ]
-  |> Enum.map(&fixture/1)
-  |> Enum.each(&Code.require_file/1)
+  describe "load/1 without a theme or plugin" do
+    setup :do_common_setup
 
-  describe "load/1" do
-    setup do
-      tmp_dir = get_tmp_dir("serum_test_")
-
-      File.mkdir_p!(tmp_dir)
-
-      on_exit(fn ->
-        File.rm_rf!(tmp_dir)
-        Plugin.load_plugins([])
-        Theme.load(nil)
-      end)
-
-      {:ok, tmp_dir: tmp_dir}
+    test "loads include files", %{dir: dir} do
+      assert {:ok, files} = IncludeLoader.load(dir)
+      assert length(files) === 3
     end
 
-    test "loads include files without a theme", %{tmp_dir: tmp_dir} do
-      includes_dir = Path.join(tmp_dir, "includes")
+    test "fails when some files cannot be loaded", ctx do
+      foo_include_path = Path.join(ctx.includes_dir, "foo.html.eex")
 
-      make_files(includes_dir)
+      File.chmod!(foo_include_path, 0o000)
 
-      assert {:ok, files} = load(tmp_dir)
-      assert 3 === length(files)
+      assert {:error, %Error{}} = IncludeLoader.load(ctx.dir)
+
+      File.chmod!(foo_include_path, 0o644)
     end
 
-    test "loads include files with a theme", %{tmp_dir: tmp_dir} do
-      includes_dir = Path.join(tmp_dir, "includes")
-      theme_dir = get_tmp_dir("serum_test_")
+    test "does not fail even if the includes directory does not exist", ctx do
+      File.rm_rf!(ctx.includes_dir)
 
-      make_files(includes_dir)
-      File.mkdir_p!(theme_dir)
-      File.mkdir_p!(Path.join(theme_dir, "includes"))
-
-      ~w(lorem ipsum)
-      |> Enum.map(&(&1 <> ".html.eex"))
-      |> Enum.map(&Path.join([theme_dir, "includes", &1]))
-      |> Enum.each(&File.touch!/1)
-
-      {:ok, _} = Theme.load(Serum.RealDummyTheme)
-      {:ok, agent} = Agent.start_link(fn -> theme_dir end, name: Serum.TestAgent)
-
-      assert {:ok, files} = load(tmp_dir)
-      assert 5 === length(files)
-
-      :ok = Agent.stop(agent)
-
-      File.rm_rf!(theme_dir)
-    end
-
-    test "succeeds even if the includes directory does not exist", %{tmp_dir: tmp_dir} do
-      assert {:ok, []} === load(tmp_dir)
-    end
-
-    test "fails when some files cannot be loaded", %{tmp_dir: tmp_dir} do
-      includes_dir = Path.join(tmp_dir, "includes")
-      foo = Path.join(includes_dir, "foo.html.eex")
-
-      make_files(includes_dir)
-      File.chmod!(foo, 0o000)
-
-      assert {:error, _} = load(tmp_dir)
-
-      File.chmod!(foo, 0o644)
-    end
-
-    test "fails when a theme fails", %{tmp_dir: tmp_dir} do
-      includes_dir = Path.join(tmp_dir, "includes")
-
-      make_files(includes_dir)
-      Theme.load(Serum.FailingTheme)
-
-      assert {:error, _} = load(tmp_dir)
-    end
-
-    test "fails when a plugin fails", %{tmp_dir: tmp_dir} do
-      plugin_mock =
-        get_plugin_mock(%{
-          {:reading_templates, 2} => fn _, _ -> raise "foo" end
-        })
-
-      includes_dir = Path.join(tmp_dir, "includes")
-      {:ok, _} = Plugin.load_plugins([plugin_mock])
-
-      make_files(includes_dir)
-
-      assert {:error, _} = load(tmp_dir)
+      assert {:ok, []} = IncludeLoader.load(ctx.dir)
     end
   end
 
-  defp make_files(dir) do
-    File.mkdir_p!(dir)
+  describe "load/1 with a theme" do
+    setup :do_common_setup
+
+    setup do
+      theme_includes_dir = get_tmp_dir("serum_test_")
+
+      File.mkdir_p!(theme_includes_dir)
+
+      theme_includes =
+        ~w(lorem ipsum)
+        |> Enum.map(&(&1 <> ".html.eex"))
+        |> Enum.map(&Path.join(theme_includes_dir, &1))
+
+      Enum.each(theme_includes, &File.touch!/1)
+
+      on_exit(fn ->
+        File.rm_rf!(theme_includes_dir)
+        ThemeLoader.load_theme(nil)
+      end)
+
+      [theme_includes: theme_includes]
+    end
+
+    test "loads include files", ctx do
+      theme_mock = get_theme_mock(%{get_includes: fn _ -> {:ok, ctx.theme_includes} end})
+      {:ok, %Theme{}} = ThemeLoader.load_theme(theme_mock)
+
+      assert {:ok, files} = IncludeLoader.load(ctx.dir)
+      assert length(files) === 5
+    end
+
+    test "returns an error if the loaded theme fails", ctx do
+      theme_mock = get_theme_mock(%{get_includes: fn _ -> raise "test: get_includes" end})
+      {:ok, %Theme{}} = ThemeLoader.load_theme(theme_mock)
+
+      assert {:error, %Error{} = error} = IncludeLoader.load(ctx.dir)
+
+      message = to_string(error)
+
+      assert message =~ "RuntimeError"
+      assert message =~ "test: get_includes"
+    end
+  end
+
+  describe "load/1 with plugins" do
+    setup :do_common_setup
+    setup do: on_exit(fn -> PluginLoader.load_plugins([]) end)
+
+    test "returns an error when loaded plugins fail", ctx do
+      plugin_mock =
+        get_plugin_mock(%{
+          {:reading_templates, 2} => fn _, _ -> raise "test: reading_templates/2" end
+        })
+
+      {:ok, [%Plugin{}]} = PluginLoader.load_plugins([plugin_mock])
+
+      assert {:error, %Error{} = error} = IncludeLoader.load(ctx.dir)
+
+      message = to_string(error)
+
+      assert message =~ "RuntimeError"
+      assert message =~ "test: reading_templates/2"
+    end
+  end
+
+  defp do_common_setup(_context) do
+    tmp_dir = get_tmp_dir("serum_test_")
+    includes_dir = Path.join(tmp_dir, "includes")
+
+    File.mkdir_p!(includes_dir)
 
     ~w(foo bar baz)
     |> Enum.map(&(&1 <> ".html.eex"))
-    |> Enum.map(&Path.join(dir, &1))
+    |> Enum.map(&Path.join(includes_dir, &1))
     |> Enum.each(&File.touch!/1)
+
+    on_exit(fn -> File.rm_rf!(tmp_dir) end)
+
+    [dir: tmp_dir, includes_dir: includes_dir]
   end
 end
