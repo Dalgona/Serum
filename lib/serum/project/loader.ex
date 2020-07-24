a8e043ecff76fd131d6b9eb359b380e5dd3c8124
Dalgona <me@dalgona.dev>
2020-07-24 22:54:16 +0900

    feat(serum): update Serum.Project.Loader

--- lib/serum/project/loader.ex
+++ lib/serum/project/loader.ex
@@ -8,15 +8,19 @@ defmodule Serum.Project.Loader do
   alias Serum.Project
   alias Serum.Project.ElixirValidator
   alias Serum.V2
-  alias Serum.V2.Error
 
   @doc """
   Detects and loads Serum project definition file from the source directory.
   """
   @spec load(binary(), binary()) :: Result.t(Project.t())
   def load(src, dest) do
-    case do_load(src) do
-      {:ok, %Project{} = proj} ->
+    Result.run do
+      file <- V2.File.read(%V2.File{src: Path.join(src, "serum.exs")})
+      value <- eval_file(file)
+      ElixirValidator.validate(value)
+      proj = Project.new(value)
+
+      :ok =
         GlobalBindings.put(:site, %{
           name: proj.site_name,
           description: proj.site_description,
@@ -26,47 +30,16 @@ defmodule Serum.Project.Loader do
           base_url: proj.base_url
         })
 
-        Result.return(%Project{proj | src: src, dest: dest})
-
-      {:error, %Error{}} = error ->
-        error
+      Result.return(%Project{proj | src: src, dest: dest})
     end
   end
 
-  @spec do_load(binary()) :: Result.t(Project.t())
-  defp do_load(src) do
-    exs_file = %V2.File{src: Path.join(src, "serum.exs")}
-
-    Result.run do
-      file <- V2.File.read(exs_file)
-      proj <- load_exs(file)
-
-      Result.return(proj)
-    end
-  end
-
-  @spec load_exs(V2.File.t()) :: Result.t(Project.t())
-  defp load_exs(file) do
-    with {map, _} <- Code.eval_string(file.in_data, [], file: file.src),
-         :ok <- ElixirValidator.validate(map) do
-      map |> Project.new() |> Result.return()
-    else
-      # From File.read/1:
-      {:error, reason} when is_atom(reason) ->
-        Result.fail(POSIX: [reason], file: file)
-
-      # From ElixirValidator.validate/2:
-      {:invalid, message} when is_binary(message) ->
-        Result.fail(Simple: [message], file: file)
-
-      {:invalid, messages} when is_list(messages) ->
-        errors =
-          Enum.map(messages, fn message ->
-            Result.fail(Simple: [message], file: file)
-          end)
-
-        Result.aggregate(errors, "failed to validate `serum.exs`:")
-    end
+  @spec eval_file(V2.File.t()) :: Result.t(term())
+  defp eval_file(file) do
+    file.in_data
+    |> Code.eval_string([], file: file.src)
+    |> elem(0)
+    |> Result.return()
   rescue
     e in [CompileError, SyntaxError, TokenMissingError] ->
       Result.fail(Exception: [e, __STACKTRACE__], file: file, line: e.line)
