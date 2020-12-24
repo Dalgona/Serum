3678402c94562d9b169d2b9d8046dcdd2122dbb7
Dalgona <me@dalgona.dev>
2020-12-24 15:53:04 +0900

    refactor: merge Serum.Project into Serum.Project.Loader

--- lib/serum/project/loader.ex
+++ lib/serum/project/loader.ex
@@ -4,11 +4,13 @@ defmodule Serum.Project.Loader do
   _moduledocp = "A module for loading Serum project definition files."
 
   require Serum.V2.Result, as: Result
+  import Serum.V2.Console, only: [put_err: 2]
   alias Serum.GlobalBindings
   alias Serum.StructValidator.BlogConfiguration, as: BlogValidator
   alias Serum.StructValidator.Project, as: ProjectValidator
   alias Serum.V2
   alias Serum.V2.Project
+  alias Serum.V2.Project.BlogConfiguration
 
   @doc """
   Detects and loads Serum project configuration file from the source directory.
@@ -20,7 +22,7 @@ defmodule Serum.Project.Loader do
       value <- eval_file(file)
       ProjectValidator.validate(value)
       BlogValidator.validate(value.blog)
-      project = Serum.Project.new(value)
+      project = make_project(value)
       :ok = GlobalBindings.put(:project, project)
 
       Result.return(project)
@@ -39,5 +41,45 @@ defmodule Serum.Project.Loader do
 
     e ->
       Result.from_exception(e, file: file)
+  end
+
+  @spec make_project(map()) :: Project.t()
+  defp make_project(%{base_url: base_url, blog: blog} = map) do
+    new_map = %{
+      map
+      | base_url: URI.parse(base_url),
+        blog: make_blog(blog)
+    }
+
+    struct(Project, new_map)
+  end
+
+  @spec make_blog(map() | false) :: BlogConfiguration.t() | false
+  defp make_blog(blog_map_or_false)
+  defp make_blog(false), do: false
+
+  defp make_blog(%{} = blog_map) do
+    struct(BlogConfiguration, check_list_title_tag(blog_map))
+  end
+
+  @spec check_list_title_tag(map()) :: map()
+  defp check_list_title_tag(%{} = blog_map) do
+    case blog_map[:list_title_tag] do
+      nil ->
+        blog_map
+
+      format when is_binary(format) ->
+        :io_lib.format(format, ["test"])
+        blog_map
+    end
+  rescue
+    ArgumentError ->
+      message = """
+      Invalid post list title format string (`list_title_tag`).
+      The default format string will be used instead.
+      """
+
+      put_err(:warn, String.trim(message))
+      Map.delete(blog_map, :list_title_tag)
   end
 end
