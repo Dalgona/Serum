ce42522c4bd150b610945271820681d9f181f501
Dalgona <me@dalgona.dev>
2020-12-31 01:04:33 +0900

    feat(serum): Template.Compiler.Include improvements
    - include line number information in cycle/include not found errors
    - force the argument type of include/1

--- lib/serum/template/compiler/include.ex
+++ lib/serum/template/compiler/include.ex
@@ -35,17 +35,16 @@ defmodule Serum.Template.Compiler.Include do
   defp do_expand(%Template{name: name, type: type, ast: ast} = t, context) do
     next_context = %{context | template: t, stack: [name | context.stack]}
 
-    with {:ok, _} <- check_cycle(name, context),
-         walk_result = Macro.prewalk(ast, next_context, &prewalk_fun/2),
-         {new_ast, %{error: nil} = new_context} <- walk_result do
-      new_template = %Template{t | ast: new_ast, include_resolved?: true}
+    case Macro.prewalk(ast, next_context, &prewalk_fun/2) do
+      {new_ast, %{error: nil} = new_context} ->
+        new_template = %Template{t | ast: new_ast, include_resolved?: true}
 
-      TS.put(name, type, new_template)
+        TS.put(name, type, new_template)
 
-      {new_template, new_context}
-    else
-      {:error, %Error{}} = error -> {t, %{context | error: error}}
-      {_, %{error: {:error, %Error{}} = error}} -> {t, %{context | error: error}}
+        {new_template, new_context}
+
+      {_, %{error: {:error, %Error{}} = error}} ->
+        {t, %{context | error: error}}
     end
   end
 
@@ -53,18 +52,34 @@ defmodule Serum.Template.Compiler.Include do
   defp prewalk_fun(ast, context)
   defp prewalk_fun(ast, %{error: e} = ctx) when not is_nil(e), do: {ast, ctx}
 
-  defp prewalk_fun({:include, _, [arg]} = ast, context) do
-    case TS.get(arg, :include) do
+  defp prewalk_fun({:include, meta, [arg]} = ast, context) when is_binary(arg) do
+    Result.run do
+      check_cycle(arg, context)
+      TS.get(arg, :include)
+    end
+    |> case do
       {:ok, include} ->
         {new_include, new_context} = do_expand(include, context)
 
         {quote(do: (fn -> unquote(new_include.ast) end).()), new_context}
 
       {:error, %Error{} = error} ->
-        error = %Error{error | file: context.template.source}
+        error = %Error{error | file: context.template.source, line: meta[:line]}
 
         {ast, %{context | error: {:error, error}}}
     end
+  end
+
+  defp prewalk_fun({:include, meta, [arg]} = ast, context) do
+    error =
+      Result.fail(
+        "the argument of `include/1` must be a string literal " <>
+          "without any interpolation, got: #{Macro.to_string(arg)}",
+        file: context.template.source,
+        line: meta[:line]
+      )
+
+    {ast, %{context | error: error}}
   end
 
   defp prewalk_fun(ast, context), do: {ast, context}
