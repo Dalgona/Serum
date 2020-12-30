5ee793c73cea2b62df75505213918311699832f4
Dalgona <me@dalgona.dev>
2020-12-30 11:57:44 +0900

    feat(serum): implement HTML formatting of SimpleMessage

--- lib/serum/error/format/simple_message.ex
+++ lib/serum/error/format/simple_message.ex
@@ -1,11 +1,17 @@
 defimpl Serum.Error.Format, for: Serum.V2.Error.SimpleMessage do
+  require EEx
   alias Serum.V2.Error.SimpleMessage
 
   @spec format_text(SimpleMessage.t(), non_neg_integer()) :: IO.ANSI.ansidata()
   def format_text(%SimpleMessage{text: text}, _indent), do: text
 
+  eex_file =
+    :serum
+    |> :code.priv_dir()
+    |> Path.join("build_resources/simple_message.html.eex")
+
+  EEx.function_from_file(:defp, :template, eex_file, [:message])
+
   @spec format_html(SimpleMessage.t()) :: iodata()
-  def format_html(%SimpleMessage{} = _msg) do
-    ~s(<span style="color: red;">Protocol not implemented for SimpleMessage.</span>)
-  end
+  def format_html(%SimpleMessage{} = msg), do: template(msg)
 end
