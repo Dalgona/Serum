56a436c64439d10a8e5e4722e28fba369bd2d0eb
Dalgona <me@dalgona.dev>
2021-04-03 10:29:00 +0900

    build(v1): force Microscope version to v1.3.0

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Serum.Mixfile do
     [
       {:earmark, "1.4.4"},
       {:file_system, "~> 0.2"},
-      {:microscope, "~> 1.3"},
+      {:microscope, "1.3.0", override: true},
       {:timex, "~> 3.5"},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.11", only: [:test], runtime: false},
