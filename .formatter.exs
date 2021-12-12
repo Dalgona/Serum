4197791086fc08b37474ee6d83a2be8a0a5b2e46
Dalgona <me@dalgona.dev>
2021-12-12 21:05:03 +0900

    feat(v1): add Serum.Plugin.Macros.{action,function}/1 macros

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,8 @@
 [
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: [
-    defcallback: 1
+    defcallback: 1,
+    action: 1,
+    function: 1
   ]
 ]
--- lib/serum/plugin/macros.ex
+++ lib/serum/plugin/macros.ex
@@ -3,12 +3,39 @@ defmodule Serum.Plugin.Macros do
 
   _moduledocp = "Provides macros for defining the `Serum.Plugin` module."
 
+  @spec defcallback(Macro.t()) :: Macro.t()
   defmacro defcallback({:"::", _, [{name, meta, args} = call, return]}) do
     arg_expr = quote(do: args :: term())
 
     quote do
       @callback unquote(call) :: unquote(return)
       @callback unquote({name, meta, args ++ [arg_expr]}) :: unquote(return)
+    end
+  end
+
+  @spec action(Macro.t()) :: Macro.t()
+  defmacro action(spec) do
+    client(:action, __CALLER__.module, spec)
+  end
+
+  @spec function(Macro.t()) :: Macro.t()
+  defmacro function(spec) do
+    client(:function, __CALLER__.module, spec)
+  end
+
+  @spec client(atom(), module(), Macro.t()) :: Macro.t()
+  defp client(type, calling_module, {:"::", _, [{name, meta, args} = call, return]}) do
+    arg_vars =
+      args
+      |> Enum.map(fn {:"::", _, [{var_name, _, _}, _type]} -> var_name end)
+      |> Enum.map(&Macro.var(&1, calling_module))
+
+    def_call_expr = {name, meta, arg_vars}
+    def_body_expr = {:"call_#{type}", [], [name, arg_vars]}
+
+    quote do
+      @spec unquote(call) :: unquote(return)
+      def unquote(def_call_expr), do: unquote(def_body_expr)
     end
   end
 end
