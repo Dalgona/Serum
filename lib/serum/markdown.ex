b67e9d83a72c18d92bc53a97c328b412291c440c
Dalgona <me@dalgona.dev>
2022-04-04 00:20:02 +0900

    feat: handle pretty_urls option when expanding md special syntax

--- lib/serum/markdown.ex
+++ lib/serum/markdown.ex
@@ -16,22 +16,37 @@ defmodule Serum.Markdown do
   def to_html(markdown, proj) do
     markdown
     |> Earmark.as_html!()
-    |> process_links(proj.base_url)
+    |> process_links(proj)
   end
 
-  @spec process_links(binary(), binary()) :: binary()
-  defp process_links(data, base_url) do
-    replace = &Regex.replace(&2, &1, &3)
-
+  @spec process_links(binary(), Project.t()) :: binary()
+  defp process_links(data, proj) do
     data
-    |> replace.(@re_media, fn _, attr, val ->
-      make_html_attr(attr, Path.join([base_url, "media", val]))
+    |> replace_media_links(proj)
+    |> replace_page_links(proj)
+    |> replace_post_links(proj)
+  end
+
+  @spec replace_media_links(binary(), Project.t()) :: binary()
+  defp replace_media_links(data, proj) do
+    Regex.replace(@re_media, data, fn _, attr, val ->
+      make_html_attr(attr, Path.join([proj.base_url, "media", val]))
     end)
-    |> replace.(@re_page, fn _, attr, val ->
-      make_html_attr(attr, Path.join([base_url, val <> ".html"]))
+  end
+
+  @spec replace_page_links(binary(), Project.t()) :: binary()
+  defp replace_page_links(data, proj) do
+    Regex.replace(@re_page, data, fn _, attr, val ->
+      make_html_attr(attr, Path.join([proj.base_url, val <> ".html"]))
     end)
-    |> replace.(@re_post, fn _, attr, val ->
-      make_html_attr(attr, Path.join([base_url, "posts", val <> ".html"]))
+  end
+
+  @spec replace_post_links(binary(), Project.t()) :: binary()
+  defp replace_post_links(data, proj) do
+    suffix = post_suffix(proj.pretty_urls)
+
+    Regex.replace(@re_post, data, fn _, attr, val ->
+      make_html_attr(attr, Path.join([proj.base_url, "posts", val <> suffix]))
     end)
   end
 
@@ -39,4 +54,10 @@ defmodule Serum.Markdown do
   defp make_html_attr(attr, value) do
     <<attr::binary, "=\"", value::binary, "\"">>
   end
+
+  @spec post_suffix(Project.pretty_urls()) :: binary()
+  defp post_suffix(pretty_urls)
+  defp post_suffix(true), do: ""
+  defp post_suffix(:posts), do: ""
+  defp post_suffix(_pretty_urls), do: ".html"
 end
