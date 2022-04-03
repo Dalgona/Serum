b8b5a2bd1872f3eeeadbc4fdd9246765cca3725d
Dalgona <me@dalgona.dev>
2022-04-04 00:20:02 +0900

    feat: handle pretty_urls option when expanding md special syntax

--- test/serum/markdown_test.exs
+++ test/serum/markdown_test.exs
@@ -1,58 +1,117 @@
 defmodule Serum.MarkdownTest do
   use ExUnit.Case, async: true
   alias Serum.Markdown
+  alias Serum.Project
 
-  @markdown """
-  # Hello, world!
+  describe "to_html/2 without pretty URLs" do
+    test "converts markdown into HTML" do
+      tree =
+        md_general()
+        |> Markdown.to_html(%Project{base_url: "/test_site/"})
+        |> Floki.parse_document!()
 
-  The quick brown fox jumps over the lazy dog.
+      assert [{"h1", _, [h1_text]}] = Floki.find(tree, "h1")
+      assert String.trim(h1_text) === "Hello, world!"
 
-  ## Special Syntax Test
+      [ul1, ul2, _ul3, ul4] = Floki.find(tree, "ul")
+      [img1, img2] = Floki.find(tree, "img")
 
-  - [Documentation](%page:docs/index)
-  - [My Post](%post:2019-01-01-test-post)
+      assert ul1 === ul2
+      assert img1 === img2
 
-  ![Sample picture](%media:images/sample.png)
+      ul4
+      |> Floki.find("li")
+      |> Enum.each(fn {"li", _, [child]} -> assert is_binary(child) end)
+    end
 
-  ## Special Syntax in HTML
+    test "converts markdown into HTML, without applying pretty post URLs" do
+      tree =
+        md_normal_post_urls()
+        |> Markdown.to_html(%Project{base_url: "/test_site/"})
+        |> Floki.parse_document!()
 
-  <ul>
-    <li><a href="%page:docs/index">Documentation</a></li>
-    <li><a href="%post:2019-01-01-test-post">My Post</a></li>
-  </ul>
+      [ul1, ul2, ul3] = Floki.find(tree, "ul")
 
-  Mixing markdown and HTML does not work in recent version of Earmark!
-  The following code will produce an undesired markup!
+      assert ul1 === ul2
+      assert ul1 === ul3
+    end
+  end
 
-  - <a href="%page:docs/index">Documentation</a>
-  - <a href="%post:2019-01-01-test-post">My Post</a>
+  describe "to_html/2 with pretty post URLs" do
+    test "converts markdown into HTML, applying pretty post URLs" do
+      tree =
+        md_pretty_post_urls()
+        |> Markdown.to_html(%Project{base_url: "/test_site/", pretty_urls: :posts})
+        |> Floki.parse_document!()
 
-  <img src="%media:images/sample.png" alt="Sample picture">
+      [ul1, ul2, ul3] = Floki.find(tree, "ul")
 
-  ## These won't be processed
+      assert ul1 === ul2
+      assert ul1 === ul3
+    end
+  end
 
-  - %page:docs/index
-  - %post:2019-01-01-test-post
-  - %media:images/sample.png
-  """
+  defp md_general do
+    """
+    # Hello, world!
 
-  test "to_html" do
-    tree =
-      @markdown
-      |> Markdown.to_html(%{base_url: "/test_site/"})
-      |> Floki.parse_document!()
+    The quick brown fox jumps over the lazy dog.
 
-    assert [{"h1", _, [h1_text]}] = Floki.find(tree, "h1")
-    assert String.trim(h1_text) === "Hello, world!"
+    ## Special Syntax Test
 
-    [ul1, ul2, ul3, _ul4] = Floki.find(tree, "ul")
-    [img1, img2] = Floki.find(tree, "img")
+    - [Documentation](%page:docs/index)
+    - [My Post](%post:2019-01-01-test-post)
 
-    assert ul1 === ul2
-    assert img1 === img2
+    ![Sample picture](%media:images/sample.png)
 
-    ul3
-    |> Floki.find("li")
-    |> Enum.each(fn {"li", _, [child]} -> assert is_binary(child) end)
+    ## Special Syntax in HTML
+
+    <ul>
+      <li><a href="%page:docs/index">Documentation</a></li>
+      <li><a href="%post:2019-01-01-test-post">My Post</a></li>
+    </ul>
+
+    Mixing markdown and HTML does not work in recent version of Earmark!
+    The following code will produce an undesired markup!
+
+    - <a href="%page:docs/index">Documentation</a>
+    - <a href="%post:2019-01-01-test-post">My Post</a>
+
+    <img src="%media:images/sample.png" alt="Sample picture">
+
+    ## These won't be processed
+
+    - %page:docs/index
+    - %post:2019-01-01-test-post
+    - %media:images/sample.png
+    """
+  end
+
+  defp md_normal_post_urls do
+    """
+    - [My Post](%post:2019-01-01-test-post)
+
+    <ul>
+      <li><a href="%post:2019-01-01-test-post">My Post</a></li>
+    </ul>
+
+    <ul>
+      <li><a href="/test_site/posts/2019-01-01-test-post.html">My Post</a></li>
+    </ul>
+    """
+  end
+
+  defp md_pretty_post_urls do
+    """
+    - [My Post](%post:2019-01-01-test-post)
+
+    <ul>
+      <li><a href="%post:2019-01-01-test-post">My Post</a></li>
+    </ul>
+
+    <ul>
+      <li><a href="/test_site/posts/2019-01-01-test-post">My Post</a></li>
+    </ul>
+    """
   end
 end
